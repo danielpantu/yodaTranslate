49d31ade4418e20df608e695628bb9e4a765735b
Daniel Pantu <danielpantu.code@gmail.com>
2022-12-09 17:02:29 -0300

    added inital code

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-
+// Shows the translation result text on the translation Output area.
 const showTranslatedText = (translatedText) => {
-//    console.log(translatedText)
     let translation = document.getElementById("textOutput")
     translation.innerText = translatedText.contents.translated
 };
-
+// Uses encoded text to concat user's input to the API URL after encoding it.
 const translateText = (text) => {
     let encodedText = encodeURIComponent(text)
-//    console.log(encodedText)
     fetch (`https://api.funtranslations.com/translate/yoda.json?text=${encodedText}`)
     .then(response => response.json())
         .then(data => showTranslatedText(data))
     .catch(error => console.log(error.error.message))
 };
-
+// Add's "click" event listener to the "translate" button.
 let button = document.getElementById("translateButton")
 button.addEventListener("click", event => {
     let textInput = document.getElementById("textInput").value
